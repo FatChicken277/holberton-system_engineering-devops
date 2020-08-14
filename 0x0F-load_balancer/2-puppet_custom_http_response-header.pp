fbe1fe6078e0795046c4de0ea874f73c373ce0f8
FatChicken277 <1637@holbertonschool.com>
2020-08-14 14:50:28 -0500

    added 2-puppet_custom_http_response-header.pp file

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,22 @@
+# install nginx
+
+package { 'nginx':
+  ensure   => 'latest',
+  name     => 'nginx',
+  provider => 'apt'
+}
+
+# custom header
+
+file_line { 'custom_header':
+  path  => '/etc/nginx/sites-available/default',
+  line  => "\tadd_header X-Served-By $hostname;}",
+  after => '^server {',
+}
+
+# start server.
+
+service { 'nginx':
+  ensure => running,
+  enable => true
+}
