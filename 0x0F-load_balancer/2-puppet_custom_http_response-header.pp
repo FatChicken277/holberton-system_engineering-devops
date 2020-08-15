4707791e19dd839e8821dd1098e39e7378ce954f
FatChicken277 <1637@holbertonschool.com>
2020-08-14 19:07:28 -0500

    added update

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,16 +1,22 @@
 # install nginx
 
+exec { 'update':
+  command => 'sudo apt-get update',
+  path    => ['/usr/bin', '/bin'],
+}
+
 package { 'nginx':
   ensure   => 'installed',
   name     => 'nginx',
   provider => 'apt',
+  require  => Exec['update'],
 }
 
 # custom header
 
 file_line { 'custom_header':
   path    => '/etc/nginx/sites-available/default',
-  line    => "\tadd_header X-Served-By \$hostname;",
+  line    => "\tadd_header X-Served-By ${hostname};",
   after   => 'listen 80 default_server;',
   require => Package['nginx'],
 }
@@ -19,5 +25,5 @@ file_line { 'custom_header':
 
 exec { 'restart':
   command => 'sudo service nginx restart',
-  path    => ['/usr/bin', '/bin']
+  path    => ['/usr/bin', '/bin'],
 }
