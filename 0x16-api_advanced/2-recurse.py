4e8ce82108c8dfe21536fb8c22d80affec502550
FatChicken277 <1637@holbertonschool.com>
2020-09-17 14:07:17 -0500

    added 2-recurse.py file

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This Module is responsible for returns a list containing the titles of
+    all hot articles for a given subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """This method returns a list containing the titles of all
+        hot articles for a given subreddit"""
+    headers = {"user-agent": "1637-holberton"}
+    if after is None:
+        return hot_list
+    if after != "":
+        r = requests.get(
+            'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=headers)
+    else:
+        r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=headers)
+    if r.status_code != 200:
+        return None
+    after = r.json().get("data").get("after")
+    for child in r.json().get("data").get("children")[:10]:
+        hot_list.append(child.get("data").get("title"))
+    return(recurse(subreddit, hot_list, after))
