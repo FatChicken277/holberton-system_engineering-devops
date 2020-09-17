55e12031382e595c25a364d22a555639658b6083
FatChicken277 <1637@holbertonschool.com>
2020-09-17 15:59:01 -0500

    added 100-count.py file

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module is in charge of making the connection with the
+    api and parses the title of all hot articles, and prints a
+    sorted count of given keywords."""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """This method returns a list containing the titles of all
+        hot articles for a given subreddit"""
+    headers = {"user-agent": "1637-holberton"}
+    if after is None:
+        return hot_list
+    if after == "":
+        r = requests.get('https://www.reddit.com/r/{}/hot.json'.format(
+            subreddit), headers=headers, allow_redirects=False)
+    else:
+        r = requests.get(
+            'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+                subreddit, after), headers=headers, allow_redirects=False)
+    if r.status_code != 200:
+        return None
+    after = r.json().get("data").get("after")
+    for child in r.json().get("data").get("children"):
+        hot_list.append(child.get("data").get("title"))
+    return recurse(subreddit, hot_list, after)
+
+
+def count_words(subreddit, word_list):
+    """This method parses the title of all hot articles,
+        and prints a sorted count of given keywords."""
+    titles = recurse(subreddit)
+    if titles is None:
+        return
+    dictionary = {}
+    for word in word_list:
+        if sum(word in title for title in titles) > 0:
+            dictionary[word] = sum(word in title for title in titles)
+    for w in sorted(dictionary, key=dictionary.get, reverse=True):
+        print("{}: {}".format(w, dictionary[w]))
