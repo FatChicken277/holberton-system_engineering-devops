d91e428f21f85fd68f15f2763211cc67d8d19f0b
FatChicken277 <1637@holbertonschool.com>
2020-10-08 16:51:03 -0500

    File 0-the_sky_is_the_limit_not.pp added

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# This script fix the file limit.
+include stdlib
+
+file_line { 'fix_file_limit':
+  path   => '/etc/default/nginx',
+  line   => 'ULIMIT="-n 1536"',
+  match  => '^ULIMIT=',
+}
+
+exec { 'restart':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
+  require => File_line['fix_file_limit'],
+}
