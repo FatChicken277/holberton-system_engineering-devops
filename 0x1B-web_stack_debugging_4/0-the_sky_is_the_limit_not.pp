afaa04ba010139902d4b9e1df8ba075591d4cdcc
FatChicken277 <1637@holbertonschool.com>
2020-10-08 17:01:08 -0500

    fix puppet-lint requirements

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,13 +2,13 @@
 include stdlib
 
 file_line { 'fix_file_limit':
-  path   => '/etc/default/nginx',
-  line   => 'ULIMIT="-n 1536"',
-  match  => '^ULIMIT=',
+  path  => '/etc/default/nginx',
+  line  => 'ULIMIT="-n 1536"',
+  match => '^ULIMIT=',
 }
 
 exec { 'restart':
-  command => 'sudo service nginx restart',
+  command => 'service nginx restart',
   path    => ['/usr/bin', '/bin'],
   require => File_line['fix_file_limit'],
 }
