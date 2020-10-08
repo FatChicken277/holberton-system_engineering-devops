c507c67e487f37433cdb0720a3b6d2ee9b560339
FatChicken277 <1637@holbertonschool.com>
2020-10-08 18:27:11 -0500

    add service restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # This script fix the file limit.
 exec { 'fix_file_limit':
-  command => 'sed -i /ULIMIT/d /etc/default/nginx',
-  path    => '/bin',
+  command => 'sed -i /ULIMIT/d /etc/default/nginx && service nginx restart',
+  path    => '/bin:/usr/bin',
 }
